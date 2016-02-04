9df3cabc8d1483a9116f7d0de30e631e912d3121
keawade <keawade@gmail.com>
2016-02-03 19:02:58 -0600

    Basic site implemented

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var express = require('express')
 
 var app = express()
+var routes = require('./routes/')
 
-// TODO - Finish this stuff
+app.set('view engine', 'jade')
+app.set('port', process.env.PORT || 3000)
+app.set('ip', process.env.IP || 'localhost')
+
+app.use(express.static('public'))
+app.locals.sitename = 'Security'
+app.locals.source_url = 'https://github.com/UnionTTC/security'
+
+app.use(routes)
+
+var server = app.listen(app.get('port'), app.get('ip'), function () {
+  var address = server.address()
+  console.log('[server.js] app running at http://%s:%s', address.address, address.port)
+})
