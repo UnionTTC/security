1c8f208da143088fd2a9d086d143874f4028618c
greenitthe <greenitthe@gmail.com>
2016-02-05 15:00:02 -0600

    Tweaked header navbar

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,42 @@ var router = module.exports = express.Router()
 
 router.get('/', function (req, res) {
   res.render('index', {
-    title: 'Thing!'
+    title: 'Home',
+    username: 'temp - no user'
   })
 })
 
-router.get('/diff', function (req, res) {
+router.get('/form1', function (req, res) {
   res.render('index', {
-    title: 'different'
+    title: '1st Form',
+    username: 'temp - no user'
+  })
+})
+
+router.get('/form2', function (req, res) {
+  res.render('index', {
+    title: '2st Form',
+    username: 'temp - no user'
+  })
+})
+
+router.get('/form3', function (req, res) {
+  res.render('index', {
+    title: '3st Form',
+    username: 'temp - no user'
+  })
+})
+
+router.get('/incident-report', function (req, res) {
+  res.render('index', {
+    title: 'Incident Report',
+    username: 'temp - no user'
+  })
+})
+
+router.get('/other-report', function (req, res) {
+  res.render('index', {
+    title: 'Other Report',
+    username: 'temp - no user'
   })
 })
