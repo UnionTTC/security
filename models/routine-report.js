b387219cda562a7e59c09a29c7439a92cecdfb54
Keith Wade <keawade@gmail.com>
2016-02-11 17:32:42 -0600

    Model update

--- models/routine-report.js
+++ models/routine-report.js
@@ -4,9 +4,14 @@ var routineSchema = new mongoose.Schema({
   team: [String],
   time: String,
   date: String,
-  location: String,
+  building: String,
+  issue: Boolean, // locked unlocked occupied
   notes: String
 })
+
+// cookie to keep track of position
+// Close progression after 3 hours
+// Fail state notification
 
 var routine = mongoose.model('routine', routineSchema)
 module.exports = routine
